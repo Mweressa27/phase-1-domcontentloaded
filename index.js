eabf663d1a4e9e63315191d0e4019bda9a6d2af1
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-03-18 08:46:51 +0300

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+/* document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+})
+
+console.log("The DOM has not loaded") */
+
+document.addEventListener("DOMContentLoaded", function() {
+    const p = document.querySelector('#text')
+    p.textContent = "This is really cool!"
+    console.log(p)
+})
